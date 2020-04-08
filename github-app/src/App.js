62269d6f7139c7d5b4eb0589cb91a96bc576ab6c
mosaelitsoane@gmail.com <mosaelitsoane@Mosaes-MacBook-Pro.local>
2020-04-08 00:26:30 -0500

    Created DisplayUserDataComponent

---
+++ github-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import DisplayUserData from './components/DisplayUserData';
+
+function App() {
+	return (
+		<div className="App">
+			<h1>Git Hub User card</h1>
+
+			<DisplayUserData />
+		</div>
+	);
+}
+
+export default App;
---
+++ github-app/src/components/DisplayUserData.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DisplayUserData = () => {
+	return (
+		<div className="user-card">
+			<h2>User data goes here</h2>
+		</div>
+	);
+};
+
+export default DisplayUserData;
